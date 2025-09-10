f0ef29e152191c88ce15101f82868703d69da493
tangaac <tangyan01@loongson.cn>
2025-09-10 10:33:00 +0800

    temp

---
+++ scripts/diff-pr.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+BEFORE_BRANCH=pr-$1-before
+AFTER_BRANCH=pr-$1-after
+
+rm -rf $RESULTS_DIR
+mkdir -p $RESULTS_DIR
+
+
+git config user.name "github-actions[bot]"
+git config user.email "41898282+github-actions[bot]@users.noreply.github.com"
+
+# BEFORE
+git checkout -b $BEFORE_BRANCH
+
+cd $BUILDS_DIR
+diff -rq before/ after/ | rg differ | awk '{print $2}' | while read -r file; do
+    dest="$RESULTS_DIR/${file#before/}"
+    mkdir -p "$(dirname "$dest")"
+    cp "$file" "$dest"
+done
+
+cd $RESULTS_DIR
+git add -A
+git commit -m "files before pr $1"
+git push --set-upstream origin $BEFORE_BRANCH
+
+
+# AFTER
+git checkout -b $AFTER_BRANCH
+
+cd $BUILDS_DIR
+diff -rq before/ after/ | rg differ | awk '{print $4}' | while read -r file; do
+    dest="$RESULTS_DIR/${file#after/}"
+    mkdir -p "$(dirname "$dest")"
+    cp "$file" "$dest"
+done
+
+cd $RESULTS_DIR
+git add -A
+git commit -m "files after pr $1"
+git push --set-upstream origin $AFTER_BRANCH
+
+
+# PR
+
+gh pr create \
+    --title "LLVM LoongArch optimization on PR $1 [bot]" \
+    --body "None"
+    --base $BEFORE_BRANCH
+    --head $AFTER_BRANCH
